d90845364e3238e51721667295f1fc0524665f62
dipeshsubedi77 <dipeshfitness74@gmail.com>
2025-03-17 16:55:26 +0545

    hhehehhehggjhj

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -254,7 +254,7 @@ const AboutSection = () => {
                 <div className="aspect-[4/5] bg-gradient-to-br from-purple-100 to-blue-100 dark:from-purple-900/30 dark:to-blue-900/30 rounded-2xl p-3 shadow-xl backdrop-blur-sm border border-white/20 dark:border-white/10">
                   <div className="w-full h-full rounded-xl overflow-hidden">
                     <img
-                      src="./dipeshsubediport\src\assets\pp.png"
+                      src="/dipeshsubediport\src\assets\pp.png"
                       alt="Dipesh Subedi"
                       className="w-full h-full object-cover"
                     />
